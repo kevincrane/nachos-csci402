b87f5526530f6024c6ee69956e4c9f03e35b9b11
Kevin Crane <kevincrane@yahoo.com>
2011-09-11 20:51:00 -0700

    Added new file and header for theater_sim. Add code there, and do it by Thursday.

---
+++ code/threads/theater_sim.cc
@@ -0,0 +1,13 @@
+// theater_sim.cc
+// All relevant agent-like methods governing the interactions within the
+// movie theater simulation.
+//
+// TODO: ADD DOCUMENTATION HERE
+
+#include "copyright.h"
+#include "system.h"
+#ifdef CHANGED
+#include "synch.h"
+#endif
+
+// CODE HERE BITCHES
---
+++ code/threads/theater_sim.h
@@ -0,0 +1,12 @@
+// theater_sim.h
+// Header file for theater_sim.cc
+//
+// TODO: add documentation here
+
+#ifndef THEATER_SIM_H
+#define THEATER_SIM_H
+
+#include "copyright.h"
+#include "utility.h"
+
+#endif // THEATER_SIM_H
