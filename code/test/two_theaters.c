77305b84cf34cfc5ae092771aa5d508f8cedef5a
Kevin Crane <kevincrane@yahoo.com>
2011-10-11 23:53:54 -0700

    Done with Part 2. Yay? Not yay.

--- code/test/two_theaters.c
+++ code/test/two_theaters.c
@@ -2,10 +2,10 @@
 
 /*Starts executing here*/
 int main() {
-  Exec("../test/theater_sim", sizeof("../test/theater_sim"));
   Write("Theater 1!\n", sizeof("Theater 1!\n"), ConsoleOutput);
   Exec("../test/theater_sim", sizeof("../test/theater_sim"));
   Write("Theater 2!\n", sizeof("Theater 2!\n"), ConsoleOutput);
+  Exec("../test/theater_sim", sizeof("../test/theater_sim"));
   
   Exit(0);
 }
